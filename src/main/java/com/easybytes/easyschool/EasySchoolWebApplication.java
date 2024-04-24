0d9a358bafb6f709e9c51b479083faf512083c64
saif786a <alamsaif86@gmail.com>
2024-04-25 00:58:45 +0530

    Adding Files

---
+++ src/main/java/com/easybytes/easyschool/EasySchoolWebApplication.java
@@ -0,0 +1,13 @@
+package com.easybytes.easyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySchoolWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySchoolWebApplication.class, args);
+	}
+
+}
