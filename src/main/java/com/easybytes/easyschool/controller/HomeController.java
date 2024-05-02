167a4de2accee116a649c5a631d9f3451f6d046d
saif786a <alamsaif86@gmail.com>
2024-05-02 23:23:12 +0530

    Front-End UI code is updated

---
+++ src/main/java/com/easybytes/easyschool/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.easybytes.easyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage(Model model)
+    {
+        model.addAttribute("username" ,"Saif Alam");
+        return "home.html";
+    }
+}
