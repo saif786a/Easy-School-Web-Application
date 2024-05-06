99d60b51beb53dd2236acb65151eeed61174d0e5
saif786a <alamsaif86@gmail.com>
2024-05-06 23:46:48 +0530

    Complete Holiday Section

---
+++ src/main/java/com/easybytes/easyschool/model/Holiday.java
@@ -0,0 +1,17 @@
+package com.easybytes.easyschool.model;
+
+import com.sun.jdi.PrimitiveValue;
+import lombok.*;
+
+@Data
+public class Holiday {
+
+    private final String day;
+    private  final  String reason;
+    private final Type type;
+
+    public enum Type{
+        FESTIVAL, FEDERAL
+    }
+}
+
