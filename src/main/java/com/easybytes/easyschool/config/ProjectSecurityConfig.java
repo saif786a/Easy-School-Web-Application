151d739325f5f31a7de16c57ff090a1053fb730a
saif786a <alamsaif86@gmail.com>
2024-05-17 23:10:33 +0530

    Done bcrypt Password security

--- src/main/java/com/easybytes/easyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/easyschool/config/ProjectSecurityConfig.java
@@ -4,9 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -36,22 +35,9 @@ public class ProjectSecurityConfig {
 
         return http.build();
     }
-
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("54321")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-
 
 }
--- src/main/java/com/easybytes/easyschool/service/PersonService.java
+++ src/main/java/com/easybytes/easyschool/service/PersonService.java
@@ -6,6 +6,7 @@ import com.easybytes.easyschool.model.Roles;
 import com.easybytes.easyschool.repository.PersonRepository;
 import com.easybytes.easyschool.repository.RolesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,10 +19,14 @@ public class PersonService {
     @Autowired
     private RolesRepository rolesRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public boolean createNewPerson(Person person){
         boolean isSaved = false;
         Roles role = rolesRepository.getByRoleName(EazySchoolConstants.STUDENT_ROLE);
         person.setRoles(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
         person = personRepository.save(person);
         if (null != person && person.getPersonId() > 0)
         {
